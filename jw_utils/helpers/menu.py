727d64de6d2e2d8d883a45dae3caff37eb0c2e36
Jay Williams <course@jaywilliams.me>
2017-10-19 19:04:14 +0100

    Fixed some formatting

--- jw_utils/helpers/menu.py
+++ jw_utils/helpers/menu.py
@@ -2,6 +2,7 @@
 # This module is included as part of https://github.com/codingJWilliams/jw_utils
 # Liscenced according to ../../LISCENCE
 # Contact codingJWilliams on github with any queries
+
 import jw_utils
 
 class Menu:
